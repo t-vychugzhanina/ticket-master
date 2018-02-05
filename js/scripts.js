e57a6d9fdd9312d587234fafbcd706af716fd893
t.vychugzhanina <t.vychugzhanina@andersenlab.com>
2018-02-05 16:39:04 +0300

    changed tags class names, added date input form for IE through jsdelivr, added code for anchor

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
 $(function() {		
-	$('.burger-icon, .burger-menu').click(function(){
+	$('.burger-icon').click(function(){
 					$('.burger-menu').toggleClass('burger-menu_opened');
+					$('.content__aside').removeClass('content__aside_opened');
 				});
-				$(document).click(function(event) {
-					if ($(event.target).closest(".burger-icon").length ) return;
-					$('.burger-menu').removeClass('burger-menu_opened');
-					event.stopPropagation();
-				});
-				
+							
 	$('.setting').click(function(){
 		$('.content__aside').toggleClass('content__aside_opened');
+		$('.burger-menu').removeClass('burger-menu_opened');
 	});
+	
+	anchorHeight = (function() {
+		var headerHeight = $('.header').outerHeight();
+        $('a').on('click', function(e) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - headerHeight
+        }, 900, 'easeInOutExpo');
+        event.preventDefault();
+		history.pushState({}, "", this.href);})	
+	});
+	
+	window.onload = anchorHeight();
+	window.onresize = anchorHeight();
+	
+	$(document).click(function(event) {
+    if ($(event.target).closest(".burger-menu").length) return;
+	if ($(event.target).closest(".burger-icon").length) return;
+	if ($(event.target).closest(".content__aside").length) return;
+	if ($(event.target).closest(".setting").length) return;
+	$('.burger-menu').removeClass('burger-menu_opened');
+	$('.content__aside').removeClass('content__aside_opened');
+    event.stopPropagation();
+});
+	
+	webshims.setOptions('waitReady', false);
+	webshims.setOptions('forms-ext', {types: 'date'});
+	webshims.polyfill('forms forms-ext');
 	
 	Date.prototype.addDays = (function(days) {
                 var dat = new Date(this.valueOf());
